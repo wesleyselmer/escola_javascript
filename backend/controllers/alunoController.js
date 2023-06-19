7bc79e2512e236ca04900a896f90f915483bf56c
Wesley Selmer <wesley.selmer@gmail.com>
2023-06-19 13:24:05 -0300

    Backend final

--- backend/controllers/alunoController.js
+++ backend/controllers/alunoController.js
@@ -1,27 +1,87 @@
-const AlunoModel = require("../models/Aluno");
+const {Aluno: AlunoModel} = require("../models/Aluno");
 
 const alunoController = {
     create: async(req, res) => {
         try {
             const aluno = {
-                aluno   : req.body.aluno,
+                nome   : req.body.nome,
                 cpf     : req.body.cpf,
                 turma   : req.body.turma,
                 turno   : req.body.turno,
                 escola  : req.body.escola,
             };
 
-            if(aluno.escola) {
-                
+            if(!aluno.escola) {
+                res.status(204).json({msg: "Escola não encontrada."})    
             }
 
             const response = await AlunoModel.create(aluno);
-
-            res.status(201).json({ response, msg: "Serviço criado com sucesso!" })
+            res.status(201).json({ response, msg: "Aluno criado com sucesso!" })
 
         } catch (error) {
             console.log(error)
         }
+    },
+    getAll: async(req, res) => {
+        try {
+            const alunos = await AlunoModel.find();
+
+            res.json(alunos);  
+        } catch (error) {
+            console.log(error) 
+        }
+    },
+    get: async(req, res) => {
+        try {
+            const id = req.params.id
+            const aluno = await AlunoModel.findById(id)
+
+            if(!aluno) {
+                res.status(404).json({msg: "Aluno não encontrado"});
+                return
+            }
+            
+            res.json(aluno);
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    delete: async(req, res) => {
+        try {
+            const id = req.params.id;
+
+            const aluno = await AlunoModel.findById(id);
+            if(!aluno) {
+                res.status(404).json({msg: "Aluno não encontrado"});
+                return
+            }
+
+            const deletedAluno = await AlunoModel.findByIdAndDelete(id);
+
+            res.status(200).json({deletedAluno, msg: "Escola excluída com sucesso!"})
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    update: async(req, res) => {
+        const id = req.params.id;
+        const aluno = {
+            nome   : req.body.nome,
+            cpf     : req.body.cpf,
+            turma   : req.body.turma,
+            turno   : req.body.turno,
+            escola  : req.body.escola,
+        };
+
+        const updatedAluno = await AlunoModel.findByIdAndUpdate(id, aluno);
+
+        if(!updatedAluno) {
+            res.status(404).json({msg: "Aluno não encontrado."});
+            return;
+        }
+
+        res.status(200).json({aluno, msg: "Aluno atualizado com sucesso."});
     }
 
 };
--- backend/routes/alunos.js
+++ backend/routes/alunos.js
@@ -3,5 +3,9 @@ const router = require("express").Router()
 const alunoController = require("../controllers/alunoController");
 
 router.route("/alunos").post((req, res) => alunoController.create(req, res));
+router.route("/alunos").get((req, res) => alunoController.getAll(req, res));
+router.route("/alunos/:id").get((req, res) => alunoController.get(req, res));
+router.route("/alunos/:id").delete((req, res) => alunoController.delete(req, res));
+router.route("/alunos/:id").put((req, res) => alunoController.update(req, res));
 
 module.exports = router;
